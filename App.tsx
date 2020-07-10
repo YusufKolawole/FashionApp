1eb87baddd89a4412dd6b8a17035a87c2d2fdf17
AbdulQuadr <quadyusuf@gmail.com>
2020-07-10 11:28:41 +0100

    eslint and tsconfig setup and first screen completely setup

---
+++ App.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const fonts = {
+  "SFProText-Bold": require("./assets/fonts/SF-Pro-Text-Bold.otf"),
+  "SFProText-Semibold": require("./assets/fonts/SF-Pro-Text-Semibold.otf"),
+  "SFProText-Regular": require("./assets/fonts/SF-Pro-Text-Regular.otf"),
+};
+
+import { Onboarding } from "./src/Authentication";
+import { LoadAssets } from "./src/components";
+
+const AuthenticationStack = createStackNavigator();
+
+const AuthenticationNavigator = () => {
+  return (
+    <AuthenticationStack.Navigator headerMode="none">
+      <AuthenticationStack.Screen name="Onboarding" component={Onboarding} />
+    </AuthenticationStack.Navigator>
+  );
+};
+export default function App() {
+  return (
+    <LoadAssets {...{ fonts }}>
+      <AuthenticationNavigator />
+    </LoadAssets>
+  );
+}
